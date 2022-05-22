0e3adac166f0955eeaed9668bbe15f4edcc11421
im-segin <segin.bytes@gmail.com>
2022-05-22 20:06:47 +0530

    .

---
+++ blink_m328/main.c
@@ -0,0 +1,23 @@
+/*
+ * main.c
+ *
+ *  Created on: 22-May-2022
+ *      Author: bot
+ */
+
+# include <avr/io.h>
+# include <util/delay.h>
+
+int main(void)
+{
+	DDRB = 0x01;
+	PORTB = 0;
+
+	while(1)
+	{
+		PORTB = 0x01;
+		_delay_ms(100);
+		PORTB = 0x00;
+		_delay_ms(100);
+	}
+}
